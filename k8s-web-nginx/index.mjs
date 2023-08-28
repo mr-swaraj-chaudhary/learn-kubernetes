e1529b86a7c570a10548edae6042fea22b6d541e
Swaraj Kumar Chaudhary <sk255251@teradata.com>
2023-08-28 12:14:01 +0530

    Bug fix for nginx endpoint

--- k8s-web-nginx/index.mjs
+++ k8s-web-nginx/index.mjs
@@ -1,17 +1,19 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import os from 'os'
 
 const app = express()
 const PORT = 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello!, I'm nginx app")
+    res.send(`Hello!, I'm ${os.hostname()}`)
 })
 
 app.get("/nginx", async (req, res) => {
     const url = 'http://nginx'
     const response = await fetch(url)
-    res.send(response.text())
+    const body = await response.text()
+    res.send(body)
 })
 
 app.listen(PORT, () => {
